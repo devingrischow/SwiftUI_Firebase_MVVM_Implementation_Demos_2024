c23f2e6cce4e73f31c144dcc5a58da2de475a791
devingrischow <devingrischow@outlook.com>
2024-03-04 19:08:25 -0600

    Initial Commit

---
+++ SwiftUI_Firebase_MVVM_Implementation_Demos_2024/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_Firebase_MVVM_Implementation_Demos_2024
+//
+//  Created by Devin Grischow on 3/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_Firebase_MVVM_Implementation_Demos_2024/SwiftUI_Firebase_MVVM_Implementation_Demos_2024App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Firebase_MVVM_Implementation_Demos_2024App.swift
+//  SwiftUI_Firebase_MVVM_Implementation_Demos_2024
+//
+//  Created by Devin Grischow on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Firebase_MVVM_Implementation_Demos_2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
